a8dc18b01081b52556f366786b5623bf9269bc87
StarLord <94403593+starlordxe@users.noreply.github.com>
2022-04-25 11:36:57 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Prayer Of The Refugee",
+    artist: "Rise Against",
+    cover: "Rise Against - Prayer Of The Refugee.jpg",
+    file: "Rise Against - Prayer Of The Refugee.mp3",
+  },
+  {
+    title: "Snowman",
+    artist: "Sia",
+    cover: "Sia - Snowman.jpg",
+    file: "Sia - Snowman.mp3",
+  },
+  {
+    title: "Viva La Vida",
+    artist: "Coldplay",
+    cover: "Coldplay - Viva La Vida.jpg",
+    file: "Coldplay - Viva La Vida.mp3",
+  },
+];
